144cdf0cd162b78d67cbc69b100444fc3e436f41
neel2751 <patelneel1732@gmail.com>
2021-07-24 16:54:08 +0530

    test

--- checkout.php
+++ checkout.php
@@ -3,26 +3,31 @@
 require_once('./stripe/init.php');
 
 \Stripe\Stripe::setApiKey('sk_test_51JFXAbSGkyW1mtgfB7szsploYLMtaPMZChBA5PcF0egUZMsCAANE0S2HEUoSjLiZls1xF0vgPOV5IXcu5wb9p0co00FVAvxQcP');
+\Stripe\Stripe::setApiKey('sk_test_tR3PYbcVNZZ796tH88S4VQ2u');
 
+$intent = \Stripe\PaymentIntent::create([
+    'amount' => 1099,
+    'currency' => 'inr',
+]);
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-try {
-    // retrieve JSON from POST body
-    $json_str = file_get_contents('php://input');
-    $json_obj = json_decode($json_str);
+// try {
+//     // retrieve JSON from POST body
+//     $json_str = file_get_contents('php://input');
+//     $json_obj = json_decode($json_str);
 
-    $paymentIntent = \Stripe\PaymentIntent::create([
-        'paymentMethodType' => 'card',
-        'currency' => 'inr',
-    ]);
+//     $paymentIntent = \Stripe\PaymentIntent::create([
+//         'paymentMethodType' => 'card',
+//         'currency' => 'inr',
+//     ]);
 
-    $output = [
-        'clientSecret' => $paymentIntent->client_secret,
-    ];
+//     $output = [
+//         'clientSecret' => $paymentIntent->client_secret,
+//     ];
 
-    echo json_encode($output);
-} catch (Error $e) {
-    http_response_code(500);
-    echo json_encode(['error' => $e->getMessage()]);
-}
+//     echo json_encode($output);
+// } catch (Error $e) {
+//     http_response_code(500);
+//     echo json_encode(['error' => $e->getMessage()]);
+// }
